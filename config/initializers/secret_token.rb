b810793c91503e10a9bec80880adcb2eb998db0e
Jesse Silliman <jksilliman@gmail.com>
2011-06-05 16:30:04 -0500

    Initial Rails 3.0 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoalaTest::Application.config.secret_token = '2daff4c1a89b5e8db8088eecf94b8e786c9d54b0750d7794d1825f2a68a5540a773df65b34dd2e935885c06a10982cc3ead7c2a03a54c929d77f768eda0585cb'
